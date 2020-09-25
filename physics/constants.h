8ff822eee1bf4b07351a2d15095b406ada081010
James Addison <addison.j@etoncollege.org.uk>
2020-09-25 19:29:22 +0100

    ,,,

--- physics/constants.h
+++ physics/constants.h
@@ -1,10 +1,10 @@
 #pragma once
 
-// honestly fuck precision at this point
+// honestly screw precision at this point
 //
 // you should've seen the linker errors
 //
-// shit ton of multiple definitions
+// loads of multiple definitions
 
 #define CONST_G 0.00000000006673;
 #define M2_APPROX_KG 200;
