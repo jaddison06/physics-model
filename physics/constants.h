974c52f0e6dd2a55e8b164c2a7db5909e2cca2a8
James Addison <addison.j@etoncollege.org.uk>
2020-09-25 19:32:03 +0100

    somebody stop me i'm too good at this

--- physics/constants.h
+++ physics/constants.h
@@ -6,6 +6,7 @@
 //
 // loads of multiple definitions
 
-#define CONST_G 0.00000000006673;
-#define M2_APPROX_KG 200;
-#define PI 3.14159265359;
+// floats?
+#define CONST_G 0.00000000006673
+#define M2_APPROX_KG 200
+#define PI 3.14159265359
